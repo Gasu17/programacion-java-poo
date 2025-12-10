68eeebb5faf6929938f886b1d1a9b90f58174aa0
Gasu17 <sbrofati.alessandro@gmail.com>
2025-12-10 13:27:22 +0100

    Añadiendo los metodos, etc

---
+++ src/main/java/com/alessandro/poo/redsocial/ProfileStatus.java
@@ -0,0 +1,7 @@
+package com.alessandro.poo.redsocial;
+
+public enum ProfileStatus {
+
+	ACTIVE, INACTIVE , BANNED
+
+}
--- src/main/java/com/alessandro/poo/redsocial/profile.java
+++ src/main/java/com/alessandro/poo/redsocial/profile.java
@@ -1,23 +1,57 @@
 package com.alessandro.poo.redsocial;
 
-public class profile {
-	
-private String username ;
-private String displayName;
-private String biography;
-private String location;
-private int followers;
-private int post;
-private boolean state;
-private boolean verifiedAccount;
+import java.util.Scanner;
 
+public class Profile {
 
-public void showInfo() {
-	System.out.println(this.username);
+	private String username;
+	private String displayName;
+	private String biography;
+	private String location;
+	private int followers;
+	private int post;
+	private boolean verifiedAccount;
+	private ProfileStatus status;
 
-	
-}
+	public void showInfo() {
+		System.out.println(this.username);
 
+	}
 
+	public void addFollowers() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce la cantidad de seguidores");
+		int amount = 0;
+		try {
+			amount = sc.nextInt();
+		} catch (NumberFormatException e) {
+			System.out.println("Introduce un formato valido ");
+		}
+		followers = followers + amount;
+	}
+
+	public void profileStatus(String Status) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce el estado deseado");
+		String value = sc.nextLine();
+		try {
+			this.status = ProfileStatus.valueOf(value.toUpperCase());
+		} catch (IllegalArgumentException e) {
+			System.out.println("Estado no valido. Usa: ACTIVE, INACTIVE o BANNED");
+		}
+
+	}
+	public boolean checkStatus () {
+		if (this.status == ProfileStatus.ACTIVE) {
+			return true;
+
+		} else {
+			return false;
+		}
+		
+	}
+	public boolean changeStatus(checkStatus) {
+		
+	}
 
 }
